87c2803df8de0aa20bbad24f02fb2d99790a7283
+ <+>
2023-02-09 20:18:03 -0700

    P1 array find method complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+
+
+function superbowlWin(array) {
+  let obj = array;
+  let winningYear;
+  for (let eachObj in obj) {
+    let singleObj = obj[eachObj]
+    if (find(singleObj) === true) {
+      winningYear = singleObj["year"]
+      break
+    }
+  } return winningYear;
+}
+
+function find(array) {
+  if (array["result"] === "W") {
+    return true
+  } else {
+    return false
+  }
+}
+
